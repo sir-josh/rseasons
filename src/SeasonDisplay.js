cb072944721048d45ec55692439224b2e1997db3
Aniekwe Joshua <joshuaaniekwe@gmail.com>
2022-04-19 21:45:28 +0100

    add css for SeasonDisplay component

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,9 +1,10 @@
+import "./SeasonDisplay.css";
 import React from "react";
 
 const seasonConfig = {
     winter: {
         screenText: "Burr, it is chilly",
-        iconName: "snowflakes"
+        iconName: "snowflake"
     },
     summer: {
         screenText: "Let's hit the beach",
@@ -12,11 +13,14 @@ const seasonConfig = {
 }
 
 const getSeason = (lat, month) => {
+
+
     if (month > 2 && month < 9){
         return lat > 0 ? 'summer': 'winter';
     } else {
         return lat > 0 ? 'winter': 'summer';
     }
+
 }
 
 const SeasonDisplay = props => {
@@ -24,10 +28,10 @@ const SeasonDisplay = props => {
     const { screenText, iconName} = seasonConfig[season];
 
     return (
-        <div>
-            <i className={`massive ${iconName} icon`}></i>
+        <div className={`season-display ${season}`}>
+            <i className={`icon-left massive ${iconName} icon`}></i>
             <h1>{screenText}</h1>
-            <i className={`massive ${iconName} icon`}></i>   
+            <i className={`icon-right massive ${iconName} icon`}></i>   
         </div>
     );
 }
