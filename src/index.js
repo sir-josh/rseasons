bc9226cd381d2f8d1f17a1f785184d1762b5a925
Aniekwe Joshua <joshuaaniekwe@gmail.com>
2022-04-19 15:27:38 +0100

    Initial file setup and boilerplate

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { Component } from 'react';
+import ReactDOM  from 'react-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+class App extends Component {
+    constructor(props){
+        super(props);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+        this.state = { lat: null };
+
+        window.navigator.geolocation.getCurrentPosition(
+            position => {
+                this.setState({ lat: position.coords.latitude});
+            },
+            err => console.log(err.message)
+        );
+    }
+
+    render(){
+        return <div> Hello there! lat: { this.state.lat}m</div>
+    }
+}
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
